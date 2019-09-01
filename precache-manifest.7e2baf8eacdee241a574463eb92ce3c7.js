b970855b8f3e9e5af768b8964aab39106e5d4a0f
Gilberto <peter01@hotmail.es>
2019-09-01 14:05:26 -0500

    Updates

---
+++ precache-manifest.7e2baf8eacdee241a574463eb92ce3c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460b5cbd43bd4a7bda7185be4006b25d",
+    "url": "/React-Burger-Builder/index.html"
+  },
+  {
+    "revision": "e09f70c6503ae0229d9e",
+    "url": "/React-Burger-Builder/static/css/main.bfca549f.chunk.css"
+  },
+  {
+    "revision": "585efb5c0947ade29428",
+    "url": "/React-Burger-Builder/static/js/2.87f32892.chunk.js"
+  },
+  {
+    "revision": "e09f70c6503ae0229d9e",
+    "url": "/React-Burger-Builder/static/js/main.c78ca72e.chunk.js"
+  },
+  {
+    "revision": "13b8be31a787dc8b31d1",
+    "url": "/React-Burger-Builder/static/js/runtime~main.71254c5c.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React-Burger-Builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
